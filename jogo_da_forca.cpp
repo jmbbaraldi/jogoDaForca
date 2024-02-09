cb3b0355f1e6c985b6e4b4ef438b68ccc1ee19af
João Baraldi <jmbbaraldi@gmail.com>
2024-02-09 13:19:54 -0300

    Letters already attempted

--- jogo_da_forca.cpp
+++ jogo_da_forca.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <iostream>
 #include <time.h>
+#include <cctype>
 
 using namespace std;
 
@@ -12,7 +13,7 @@ void limpaTela() {
 
 string retornaPalavraAleatoria() {
     //Vetor com palavras disponíveis
-    string palavras[3] = {"abacaxi", "manga", "morango"};
+    string palavras[3] = {"Abacaxi", "Manga", "Morango"};
 
     //Imprime ou 0 ou 1 ou 2
     int indiceAleatorio = rand() % 3;
@@ -34,11 +35,19 @@ string retornaPalavraComMascara(string palavra, int tamanhoDaPalavra) {
     return palavraComMascara;
 }
 
-void exibeStatus(string palavraComMascara, int tamanhoDaPalavra, int tentativasRestantes) {
+void exibeStatus(string palavraComMascara, int tamanhoDaPalavra, int tentativasRestantes, string letrasJaArriscadas) {
 
         //Status do jogo
         cout << "Palavra: " << palavraComMascara << " | Tamanho da palavra: " << tamanhoDaPalavra;
         cout << "\nTentativas Restantes: " << tentativasRestantes;
+
+        //Exibe as letras arriscadas
+        int i;
+        cout << "\nLetras arriscadas: ";
+        for (i = 0; i < letrasJaArriscadas.size(); i++) {
+            cout << letrasJaArriscadas[i] << ", ";
+        }
+
 }
 
 void jogarSozinho() {
@@ -56,30 +65,47 @@ void jogarSozinho() {
     int tentativas = 0, maximoDeTentativas = 5;         //Número de tentativas
     int i = 0;                                          //Auxiliar de laços de repetição
     char letra;                                         //Letra arriscada pelo usuário
+    string letrasJaArriscadas;                          //Acumula as tentativas do jogador
+    bool jaDigitouLetra = false;                        //Auxiliar para saber se a letra foi ja foi digitada
 
     while(palavra != palavraComMascara && maximoDeTentativas - tentativas > 0) {
 
         limpaTela();
 
         //Exibe o status atual do jogo
-        exibeStatus(palavraComMascara, tamanhoDaPalavra, maximoDeTentativas - tentativas);
+        exibeStatus(palavraComMascara, tamanhoDaPalavra, maximoDeTentativas - tentativas, letrasJaArriscadas);
 
         //Palpite
         cout << "\nDigite uma letra: ";
         cin >> letra;
 
-        //Percorre a palavra e se a letra existir muda a palavra com máscara
-        for(i = 0; i < tamanhoDaPalavra; i++) {
-            //Se a letra existir
-            if(palavra[i] == letra) {
-                //Faz a letra aparecer na palavra com máscara
-                palavraComMascara[i] = palavra[i];
+        //Percorre as letras já arriscadas
+        for(i = 0; i < tentativas; i++) {
+            if(letrasJaArriscadas[i] == letra) {
+                cout << "Essa letra ja foi digitada!";
+                //Atualiza o booleano
+                jaDigitouLetra = true;
             }
         }
 
-        //Aumenta uma tentativa realizada
-        tentativas++;
+        if(jaDigitouLetra == false) {
+            letrasJaArriscadas += letra;
+
+            //Percorre a palavra e se a letra existir muda a palavra com máscara
+            for(i = 0; i < tamanhoDaPalavra; i++) {
+                //Se a letra existir
+                if(palavra[i] == letra ) {
+                    //Faz a letra aparecer na palavra com máscara
+                    palavraComMascara[i] = palavra[i];
+                }
+            }
+
+            //Aumenta uma tentativa realizada
+            tentativas++;
+        }
+
     }
+
 
     if(palavra == palavraComMascara) {
         //Vitória
